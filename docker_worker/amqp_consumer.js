93c4eb70294c6fef3f2b959fe4441dcbe3ad0ae8
James Lal <james@lightsofapollo.com>
2014-02-09 00:52:00 -0800

    extract task running magic into taskrunner

--- docker_worker/amqp_consumer.js
+++ docker_worker/amqp_consumer.js
@@ -1,41 +1,13 @@
 var Consumer = require('amqpworkers/consumer');
-var JobAPI = require('./job_api');
-var Task = require('./task');
-var DockerProc = require('dockerode-process');
-var Middleware = require('middleware-object-hooks');
-
-var debug = require('debug')('taskclsuter-docker-worker:amqp_consumer');
-
 var stream = require('stream');
 var assert = require('assert');
+var taskrunner = require('./taskrunner');
 
 var MIDDLEWARES = {
   times: require('./middleware/times'),
   bufferLog: require('./middleware/buffer_log'),
   azureLiveLog: require('./middleware/azure_livelog')
 };
-
-/**
-Build the create configuration for the docker container.
-*/
-function createConfig(overrides) {
-  var opts = {
-    'Hostname': '',
-    'User': '',
-    'AttachStdin': false,
-    'AttachStdout': true,
-    'AttachStderr': true,
-    'Tty': true,
-    'OpenStdin': false,
-    'StdinOnce': false,
-    'Env': null,
-    'Volumes': {},
-    'VolumesFrom': ''
-  };
-
-  for (var key in overrides) opts[key] = overrides[key];
-  return opts;
-}
 
 function AMQPConusmer(options) {
   assert(options.docker, '.docker option is given');
@@ -52,68 +24,9 @@ AMQPConusmer.prototype = {
   Handle a message from the incoming queue.
   */
   read: function(message) {
-    // task result/output
-    var output = {
-      artifacts: {},
-      extra_info: {}
-    };
-
-    // details to send during the claim.
-    var claim = {};
-
-    var api = new JobAPI(message);
-    var task = new Task(api.job);
-    var middleware = new Middleware();
-
-    // always turn on times
-    middleware.use(MIDDLEWARES.times());
-
-    // this is mostly a debugging middleware so its off by default
-    if (task.feature('buffer_log', false)) {
-      middleware.use(MIDDLEWARES.bufferLog());
-    }
-
-    // live logging should always be on
-    if (task.feature('azure_livelog', true)) {
-      middleware.use(MIDDLEWARES.azureLiveLog());
-    }
-
-    var dockerProcess = new DockerProc(this.docker, {
-      start: task.startContainerConfig(),
-      create: task.createContainerConfig()
-    });
-
-    debug('run task', api.job);
-    return middleware.run('start', claim, task, dockerProcess).then(
-      function(claimPayload) {
-        debug('claim payload', claimPayload);
-        return api.sendClaim(claimPayload);
-      }
-    ).then(
-      function initiateExecute(value) {
-        return dockerProcess.run();
-      }
-    ).then(
-      function processRun(code) {
-
-        output.task_result = {
-          exit_status: code
-        };
-
-        return middleware.run('end', output, task, dockerProcess);
-      }
-    ).then(
-      function sendFinish(output) {
-        return api.sendFinish(output).then(
-          function() {
-            return dockerProcess.remove();
-          }
-        );
-      }
-    ).then(
+    return taskrunner(this.docker, message).then(
       null,
       function epicFail(err) {
-        // XXX: this should either nack or "finish" with an error.
         debug('FAILED to process task', err);
       }
     );
---
+++ docker_worker/taskrunner.js
@@ -0,0 +1,80 @@
+var JobAPI = require('./job_api');
+var Task = require('./task');
+var DockerProc = require('dockerode-process');
+var Middleware = require('middleware-object-hooks');
+
+var MIDDLEWARES = {
+  times: require('./middleware/times'),
+  bufferLog: require('./middleware/buffer_log'),
+  azureLiveLog: require('./middleware/azure_livelog')
+};
+
+var debug = require('debug')('taskcluster-docker-worker:taskrunner');
+
+/**
+@param {Object} request task.
+@return Promise
+*/
+function runTask(docker, request) {
+  // task result/output
+  var output = {
+    artifacts: {},
+    extra_info: {}
+  };
+
+  // details to send during the claim.
+  var claim = {};
+
+  var api = new JobAPI(request);
+  var task = new Task(api.job);
+  var middleware = new Middleware();
+
+  // always turn on times
+  middleware.use(MIDDLEWARES.times());
+
+  // this is mostly a debugging middleware so its off by default
+  if (task.feature('buffer_log', false)) {
+    middleware.use(MIDDLEWARES.bufferLog());
+  }
+
+  // live logging should always be on
+  if (task.feature('azure_livelog', true)) {
+    middleware.use(MIDDLEWARES.azureLiveLog());
+  }
+
+  var dockerProcess = new DockerProc(docker, {
+    start: task.startContainerConfig(),
+    create: task.createContainerConfig()
+  });
+
+  debug('run task', api.job);
+  return middleware.run('start', claim, task, dockerProcess).then(
+    function(claimPayload) {
+      debug('claim payload', claimPayload);
+      return api.sendClaim(claimPayload);
+    }
+  ).then(
+    function initiateExecute(value) {
+      return dockerProcess.run();
+    }
+  ).then(
+    function processRun(code) {
+
+      output.task_result = {
+        exit_status: code
+      };
+
+      return middleware.run('end', output, task, dockerProcess);
+    }
+  ).then(
+    function sendFinish(output) {
+      return api.sendFinish(output).then(
+        function() {
+          return dockerProcess.remove();
+        }
+      );
+    }
+  );
+}
+
+module.exports = runTask;
