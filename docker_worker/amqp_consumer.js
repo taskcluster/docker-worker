347108ccdf67915c37fd12d1e9785af09ca6b9a5
James Lal <james@lightsofapollo.com>
2014-02-09 01:16:02 -0800

    trim down amqp consumer

--- docker_worker/amqp_consumer.js
+++ docker_worker/amqp_consumer.js
@@ -3,12 +3,6 @@ var stream = require('stream');
 var assert = require('assert');
 var taskrunner = require('./taskrunner');
 
-var MIDDLEWARES = {
-  times: require('./middleware/times'),
-  bufferLog: require('./middleware/buffer_log'),
-  azureLiveLog: require('./middleware/azure_livelog')
-};
-
 function AMQPConusmer(options) {
   assert(options.docker, '.docker option is given');
   assert(options.amqp, '.amqp option is given');
