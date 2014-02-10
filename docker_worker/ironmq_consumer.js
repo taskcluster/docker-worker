4db019b8d348706c1e8daff4ca46a2e2057ffdab
James Lal <james@lightsofapollo.com>
2014-02-09 22:55:00 -0800

    better polling logic

--- docker_worker/ironmq_consumer.js
+++ docker_worker/ironmq_consumer.js
@@ -1,10 +1,12 @@
 var Promise = require('promise');
 var IronMQ = require('./ironmq');
+var EventEmitter = require('events').EventEmitter;
+
 var assert = require('assert');
 var taskrunner = require('./taskrunner');
 var debug = require('debug')('taskcluster-docker-worker:ironmq');
 
-var INTERVAL = 1000;
+var INTERVAL = 5000;
 
 function IronMQConsumer(options) {
   assert(options.queue, 'has queue name');
@@ -13,28 +15,48 @@ function IronMQConsumer(options) {
   this.interval = options.interval || INTERVAL;
   this.docker = options.docker;
   this.queue = new IronMQ(options.queue);
+  this.onError = this.onError.bind(this);
+
+  EventEmitter.call(this);
 }
 
 IronMQConsumer.prototype = {
+  __proto__: EventEmitter.prototype,
   interval: 0,
   timerId: null,
 
   _poll: function() {
+    debug('wait for message', this.interval);
+    clearTimeout(this.timerId);
     this.timerId = setTimeout(this.poll.bind(this), this.interval);
   },
 
+  onError: function(err) {
+    // XXX: Implement real error handling...
+    debug('error processing message', err);
+    this.poll();
+  },
+
   poll: function() {
-    // do very dumb polling for now
-    return this.queue.get({ n: 1 }).then(
+    debug('poll ping');
+
+    // attempt to fetch N messages
+    this.queue.get({ n: 1 }).then(
       function(message) {
+        // if there is no message wait interval then call poll again.
         if (!message) return this._poll();
+
         debug('pulled message', message);
         var id = message.id;
         var body = JSON.parse(message.body);
-        return this.handleMessage(id, body);
+
+        // if we have a message work it then trigger poll directly.
+        this.handleMessage(id, body).then(
+          this.poll.bind(this),
+          this.onError
+        );
+
       }.bind(this)
-    ).then(
-      this._poll.bind(this)
     );
   },
 
