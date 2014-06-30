57916fd7eb1a1a9546d4f61c7a9eff08b154f0bd
James Lal <james@lightsofapollo.com>
2014-06-30 14:55:44 -0700

    Begin moving folders around

---
+++ test/aws_metadata.js
@@ -0,0 +1,26 @@
+/**
+Stub for the aws metadata server.
+*/
+
+var app = require('koa')();
+
+function route(path) {
+  switch (path) {
+    case '/meta-data/ami-id':
+      return 'ami-333333';
+    case '/meta-data/instance-type':
+      return 'c3.xlarge';
+    case '/meta-data/placement/availability-zone':
+      return 'us-west-2';
+    case '/meta-data/instance-id':
+      return 'i-123456';
+    default:
+      throw new Error('unknown path: ' + path);
+  }
+}
+
+app.use(function* () {
+  this.body = route(this.url);
+});
+
+module.exports = app;
