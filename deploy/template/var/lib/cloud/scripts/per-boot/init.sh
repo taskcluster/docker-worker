177a7cafefb4a48262ed36a306f610956d0c105e
James Lal <james@lightsofapollo.com>
2014-09-21 11:34:03 -0700

    Spit functionality and names of docker-worker-hosts into instance/group

--- deploy/template/var/lib/cloud/scripts/per-boot/init.sh
+++ deploy/template/var/lib/cloud/scripts/per-boot/init.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -e
 
 # On startup define the new syslog hostname
-echo "\$LocalHostName $(docker-worker-host)" > /etc/rsyslog.d/1-hostname.conf
+echo "\$LocalHostName $(docker-worker-host instance)" > /etc/rsyslog.d/1-hostname.conf
 sudo service rsyslog restart
