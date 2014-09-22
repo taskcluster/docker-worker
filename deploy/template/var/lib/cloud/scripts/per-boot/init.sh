b8bf68a5ff2d2319a899a8835a8c6845f02ba4ba
James Lal <james@lightsofapollo.com>
2014-09-21 19:16:11 -0700

    restart diamond on init

--- deploy/template/var/lib/cloud/scripts/per-boot/init.sh
+++ deploy/template/var/lib/cloud/scripts/per-boot/init.sh
@@ -3,3 +3,4 @@
 # On startup define the new syslog hostname
 echo "\$LocalHostName $(docker-worker-host instance)" > /etc/rsyslog.d/1-hostname.conf
 sudo service rsyslog restart
+sudo service diamond restart
