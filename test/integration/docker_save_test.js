e42a92e1556b6cf6cc55217280eb162d9d72c6da
Edgar Chen <eggylv999@gmail.com>
2015-07-20 13:09:01 -0700

    back to a working version, default public to private

--- test/integration/docker_save_test.js
+++ test/integration/docker_save_test.js
@@ -10,10 +10,10 @@ import request from 'superagent-promise';
 import TestWorker from '../testworker';
 import zlib from 'zlib';
 
-var debug = Debug('docker-worker:test:docker-save-test');
+let debug = Debug('docker-worker:test:docker-save-test');
 
 suite('use docker-save', () => {
-  var worker;
+  let worker;
   setup(async () => {
     worker = new TestWorker(DockerWorker);
     await worker.launch();
@@ -27,12 +27,12 @@ suite('use docker-save', () => {
   });
 
   test('run, then check contents', async () => {
-    var result = await worker.postToQueue({
+    let result = await worker.postToQueue({
       payload: {
         image: 'busybox',
         command: ['/bin/sh', '-c', 'echo testString > /tmp/test.log'],
         features: {
-          dockerSave: {artifactPath: 'public/heyy.tar'}
+          dockerSave: true
         },
         maxRunTime: 5 * 60
       }
@@ -42,13 +42,13 @@ suite('use docker-save', () => {
     assert(result.run.reasonResolved === 'completed',
                  'task should be successful');
 
-    var taskId = result.taskId;
-    var runId = result.runId;
+    let taskId = result.taskId;
+    let runId = result.runId;
 
-    var signedUrl = worker.queue.buildSignedUrl(
+    let signedUrl = worker.queue.buildSignedUrl(
       worker.queue.getLatestArtifact,
       taskId,
-      'public/heyy.tar',
+      'public/dockerImage.tar',
       {expiration: 60 * 5});
 
     try {
@@ -56,7 +56,7 @@ suite('use docker-save', () => {
       await new Promise((accept, reject) => {
         https.request(signedUrl, (res) => { //take the redirect
           https.request(res.headers.location, (res) => {
-            var unzipStream = zlib.Gunzip();
+            let unzipStream = zlib.Gunzip();
             res.pipe(unzipStream).pipe(fs.createWriteStream('/tmp/dockerload.tar'));
             unzipStream.on('end', accept);
             res.on('error', (err) => reject(err));
@@ -64,43 +64,38 @@ suite('use docker-save', () => {
           res.on('error', (err) => reject(err));
         }).end();
       });
-    } catch (e) {
-      throw new Error('download from s3 failed' + taskId);
-    }
-    try {
-      var docker = new Docker(dockerOpts());
-    } catch (e) {
-      throw new Error('couldnt make docker' + taskId);
-    }
-    try {
-      var imageName = 'task/' + taskId + '/' + runId + ':latest';
+
+      let docker = new Docker(dockerOpts());
+      let imageName = 'task/' + taskId + '/' + runId + ':latest';
       await docker.loadImage('/tmp/dockerload.tar');
-      var opts = {
+      let opts = {
         AttachStdin: true,
         AttachStdout: true,
         AttachStderr: true,
         Cmd: ['cat', '/tmp/test.log'],
         Image: imageName
       };
-      var streamOpts = {
+      let streamOpts = {
         logs: true,
         stdout: true,
       };
-      var container = await docker.createContainer(opts);
+      let container = await docker.createContainer(opts);
       await container.start();
-      var stream = await container.attach(streamOpts);
-      var finished = false;
+      let stream = await container.attach(streamOpts);
+      let finished = false;
       stream.on('data', (data) => {
         assert(data.compare(new Buffer(0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x0b, //header
           0x74,0x65,0x73,0x74,0x53,0x74,0x72,0x69,0x6e,0x67,0x0a))); //testString\n
         finished = true;
       });
-      await base.testing.sleep(3000);
+      await base.testing.sleep(4000);
       assert(finished, 'did not receive any data back');
       await Promise.all([container.remove(), fs.unlink('/tmp/dockerload.tar')]);
       await docker.getImage(imageName).remove();
     } catch (e) {
-      throw new Error('couldnt do docker things' + taskId);
+      console.log(e);
+      if(e.stack) console.log(e.stack);
+      throw e;
     }
   });
 });
