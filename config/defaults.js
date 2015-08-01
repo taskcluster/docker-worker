1a72a24ea0e4c17c9284b76d3cd0613cb4bebf0a
Edgar Chen <eggylv999@gmail.com>
2015-07-31 17:32:08 -0700

    changing poll interval

--- config/defaults.js
+++ config/defaults.js
@@ -127,7 +127,7 @@ module.exports = {
 
   taskQueue: {
     // Task queue will be polled on a frequent interval for new pending tasks
-    pollInterval: 5 * 1000,
+    pollInterval: 30 * 1000,
     // If signed url for queue expires within now()+expiration, refresh queues
     expiration: 5 * 60 * 1000,
     // Number of times to retry requests to the task queue
