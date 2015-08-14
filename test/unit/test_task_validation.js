1e2788301700582c3a7d3621e71000dd39e46609
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2015-08-14 23:31:50 +0300

    Bug 1098460: test gets its own validator instance.

--- test/unit/test_task_validation.js
+++ test/unit/test_task_validation.js
@@ -1,44 +1,45 @@
 import assert from 'assert';
 import _ from 'lodash';
 
-import { validateTask } from '../../lib/util/validation';
+import base from 'taskcluster-base';
+import { PAYLOAD_SCHEMA } from '../../lib/task.js';
 
-suite('Task validation', function() {
+suite('Task validation', async function() {
+  before(async function() {
+    this.validator = await base.validator();
+    this.validator.register(require('../../schemas/payload'));
+  });
+
   test('accept valid schema', async function () {
-    let task = {
-      payload: {
-        image: 'taskcluster/test-ubuntu',
-        command: ["echo", "5"],
-        maxRunTime: 5 * 60
-      }
-    }
+    let payload = {
+      image: 'taskcluster/test-ubuntu',
+      command: ["echo", "5"],
+      maxRunTime: 5 * 60
+    };
 
-    let errors = await validateTask(task);
+    let errors = this.validator.check(payload, PAYLOAD_SCHEMA);
+    console.log(errors);
     assert(errors === null, 'Valid payload considered invalid.');
   });
-  
-  test('catch invalid schema', async function () {
-    let task = {
-      payload: {
-        image: 'taskcluster/test-ubuntu',
-        // No maxRunTime is an invalid schema.
-      }
-    }
 
-    let errors = await validateTask(task);
+  test('catch invalid schema', async function () {
+    let payload = {
+      image: 'taskcluster/test-ubuntu',
+      // No maxRunTime is an invalid schema.
+    };
+
+    let errors = this.validator.check(payload, PAYLOAD_SCHEMA);
     assert(!_.isEmpty(errors), 'Invalid payload considered valid.');
   });
 
   test('accept missing command', async function () {
-    let task = {
-      payload: {
-        image: 'taskcluster/test-ubuntu',
-        // No command provided should be ok.
-        maxRunTime: 5 * 60
-      }
-    }
+    let payload = {
+      image: 'taskcluster/test-ubuntu',
+      // No command provided should be ok.
+      maxRunTime: 5 * 60
+    };
 
-    let errors = await validateTask(task);
+    let errors = this.validator.check(payload, PAYLOAD_SCHEMA);
     assert(errors === null, 'Valid payload considered invalid.');
   });
 });
