f57eb8e6cb5fc65ad66115d67a25262aa1f022dd
Greg Arndt <garndt@mozilla.com>
2015-09-23 18:03:17 -0500

    Image handler working

--- test/image_manager_test.js
+++ test/image_manager_test.js
@@ -2,6 +2,9 @@ import assert from 'assert';
 import dockerUtils from 'dockerode-process/utils';
 import ImageManager from '../lib/docker/image_manager';
 import Docker from '../lib/docker';
+import { Index } from 'taskcluster-client';
+import { createHash } from 'crypto';
+import slugid from 'slugid';
 
 let docker = Docker();
 
@@ -22,7 +25,7 @@ suite('Image Manager', () => {
     }
   });
 
-  test('download docker image', async () => {
+  test('download docker image from registry', async () => {
     let image = 'gliderlabs/alpine:latest';
     await dockerUtils.removeImageIfExists(docker, image);
     let runtime = {
@@ -32,16 +35,86 @@ suite('Image Manager', () => {
 
     let im = new ImageManager(runtime);
     let imageId1 = await im.ensureImage(image, process.stdout);
-
-    im.docker = {};
-
-    let imageId2;
-    try {
-      imageId2 = await im.ensureImage(image, process.stdout);
-    } catch(e) {
-      assert(false, 'Image Manager should not try to download an already downloaded image');
-    }
+    let imageId2 = await im.ensureImage(image, process.stdout);
 
     assert.equal(imageId1, imageId2, 'Image IDs for the same image should be the same');
   });
+
+  test('download indexed public image', async () => {
+    let image = {
+      namespace: 'public.garndt.garbage.test-image.v1',
+      path: 'public/image.tar'
+    };
+
+    let index = new Index();
+    let {taskId} = await index.findTask(image.namespace);
+    let hashedName = createHash('md5')
+                      .update(`${taskId}${image.path}`)
+                      .digest('hex');
+
+    await dockerUtils.removeImageIfExists(docker, hashedName);
+
+    let runtime = {
+      docker: docker,
+      dockerConfig: DOCKER_CONFIG,
+      dockerVolume: '/tmp'
+    };
+
+    let im = new ImageManager(runtime);
+    let imageId = await im.ensureImage(image, process.stdout, []);
+
+    assert.ok(imageId, 'No image id was returned');
+
+
+  });
+
+  test('task not present for indexed image', async () => {
+    let image = {
+      namespace: slugid.nice(),
+      path: 'public/image.tar'
+    };
+
+    let runtime = {
+      docker: docker,
+      dockerConfig: DOCKER_CONFIG,
+      dockerVolume: '/tmp'
+    };
+
+    let im = new ImageManager(runtime);
+    try {
+      let imageId = await im.ensureImage(image, process.stdout, []);
+      assert.ok(false, 'Exception should have been thrown');
+    } catch(e) {
+      assert.ok(
+        e.message.includes('Could not find a task associated'),
+        'Error message did not appear indicating a task could not be found'
+      );
+    }
+  });
+
+  test('artifact not present for indexed image', async () => {
+    let image = {
+      namespace: 'public.garndt.garbage.test-image.v1',
+      path: 'public/image1.tar'
+    };
+
+    let runtime = {
+      docker: docker,
+      dockerConfig: DOCKER_CONFIG,
+      dockerVolume: '/tmp'
+    };
+
+    let im = new ImageManager(runtime);
+    try {
+      let imageId = await im.ensureImage(image, process.stdout, []);
+      assert.ok(false, 'Exception should have been thrown');
+    } catch(e) {
+      console.log(e);
+      assert.ok(
+        e.message.includes('Could not download image artifact'),
+        'Error message did not appear indicating an artifact could not be found'
+      );
+    }
+  });
 });
+
