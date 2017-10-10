2679a14653ad73fe3d8f587958e83064b53ded64
Ben Hearsum <bhearsum@mozilla.com>
2017-10-10 15:11:50 -0400

    Add Balrog stage host/ip to balrogvpnproxy hack.

--- src/lib/features/balrog_vpn_proxy.js
+++ src/lib/features/balrog_vpn_proxy.js
@@ -78,6 +78,7 @@ export default class BalrogVPNProxy {
           // XXX: hack for now.  Problem in taskcluster-vpn-proxy where resolv.conf
           // isn't updated when vpn'ed in so name resolution does not work.
           "aus4-admin.mozilla.org:52.26.16.60"
+          "balrog-admin.stage.mozaws.net:52.71.237.144"
         ]
       }
     });
