91c06484c4efd6986e14f6d952a11b795d53bcc8
Gregory Szorc <gps@mozilla.com>
2018-01-11 13:26:00 -0800

    Consolidate provisioning code into packages.sh
    vagrant.sh and packages.sh are near copies of each other. Keeping them
    in sync is work.
    
    This commit teaches vagrant.sh to source packages.sh and removes the
    now redundant code from vagrant.sh.
    
    In the few places where the code varied, we introduce a
    VAGRANT_PROVISION variable to determine if we're running in Vagrant mode
    and to skip actions that don't make sense there.

--- deploy/packer/base/scripts/packages.sh
+++ deploy/packer/base/scripts/packages.sh
@@ -2,7 +2,6 @@
 
 set -e -v
 
-# Keep in sync with vagrant.sh.
 DOCKER_VERSION=1.12.6-0~ubuntu-trusty
 KERNEL_VER=4.4.0-109-generic
 V4L2LOOPBACK_VERSION=0.10.0
@@ -10,7 +9,7 @@ V4L2LOOPBACK_VERSION=0.10.0
 lsb_release -a
 
 # add docker group and add current user to it
-sudo groupadd docker
+sudo groupadd -f docker
 sudo usermod -a -G docker $USER
 
 sudo apt-get update -y
@@ -35,9 +34,10 @@ sudo apt-get install -y \
     linux-image-extra-virtual \
     dkms
 
-# On paravirtualized instances, PV-GRUB looks at /boot/grub/menu.lst, which is different from the
-# /boot/grub/grub.cfg that dpkg just updated.  So we have to update menu.list manually.
-cat <<EOF | sudo tee /boot/grub/menu.lst >&2
+if [ -z "${VAGRANT_PROVISION}" ]; then
+    # On paravirtualized instances, PV-GRUB looks at /boot/grub/menu.lst, which is different from the
+    # /boot/grub/grub.cfg that dpkg just updated.  So we have to update menu.list manually.
+    cat <<EOF | sudo tee /boot/grub/menu.lst >&2
 default         0
 timeout         0
 hiddenmenu
@@ -52,7 +52,7 @@ root            (hd0)
 kernel          /boot/vmlinuz-${KERNEL_VER} root=LABEL=cloudimg-rootfs ro  single
 initrd          /boot/initrd.img-${KERNEL_VER}
 EOF
-
+fi
 
 ## Install all the packages
 sudo apt-get install -y \
@@ -92,10 +92,10 @@ sudo mv zstd /usr/bin
 cd /
 sudo rm -rf /zstd
 
-
-## Clear mounts created in base image so fstab is empty in other builds...
-sudo sh -c 'echo "" > /etc/fstab'
-
+if [ -z "${VAGRANT_PROVISION}" ]; then
+    ## Clear mounts created in base image so fstab is empty in other builds...
+    sudo sh -c 'echo "" > /etc/fstab'
+fi
 
 ## Install v4l2loopback
 cd /usr/src
